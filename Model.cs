56468f0a0457f6dd6145faffccbeb0a4a1485321
Daniel Dutra <danielfdutra@users.noreply.github.com>
2019-09-19 19:33:07 -0300

    add project

---
+++ Model.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.Model
+{
+    public class SampleDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Movement> Movements { get; set; }
+
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+        {
+        }
+    }
+
+    public class Transaction
+    {
+        [Key]
+        public string TransactionNumber { get; set; }
+        public virtual List<Movement> Movements { get; set; }
+    }
+
+    public class Movement
+    {
+        public int MovementId { get; set; }
+        public string TransactionNumber { get; set; }
+        public virtual Transaction Transaction { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleApp.Model;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+
+                var options = new DbContextOptionsBuilder<SampleDbContext>()
+                    .UseSqlite(connection).Options;
+
+                using (var context = new SampleDbContext(options))
+                {
+                    await context.Database.EnsureCreatedAsync();
+
+                    context.Transactions.Add(
+                        new Transaction
+                        {
+                            TransactionNumber = "A",
+                            Movements = new List<Movement>
+                            {
+                                new Movement {MovementId = 1},
+                                new Movement {MovementId = 2},
+                            }
+                        });
+
+                    context.SaveChanges();
+
+                    var transactionB = new Transaction { TransactionNumber = "B" };
+
+                    context.Transactions.Add(transactionB);
+
+                    context.SaveChanges();
+
+                    var movement3 = new Movement { MovementId = 3 };
+
+                    context.Movements.Add(movement3);
+
+                    context.SaveChanges();
+
+                    var movement4 = new Movement { MovementId = 4, TransactionNumber = "C" };
+
+                    context.Movements.Add(movement4); // Cause exception
+
+                    context.SaveChanges();
+
+                    foreach (var transaction in context.Transactions)
+                    {
+                        Console.WriteLine($"Transaction {transaction.TransactionNumber} has {transaction.Movements?.Count ?? 0} movements");
+                    }
+
+                    foreach (var movement in context.Movements)
+                    {
+                        Console.WriteLine($"Movement {movement.MovementId} has the TransactionNumber {movement.TransactionNumber ?? "empty"}");
+
+                    }
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
